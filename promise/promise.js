16bd12748b22092197124c50858899c8cd2e2edf
Nadiia <npodufala15@gmail.com>
2020-12-05 21:41:50 +0200

    create promise

--- promise/promise.js
+++ promise/promise.js
@@ -1,23 +1,58 @@
+function noop() {}
+
 class NadiiPromise {
     constructor(executor) {
-        executor()
-    }
-then() {}
-catch() {}
+        this.queue = [];
+        this.errorHandler = noop;
+this.finallyHandler = noop;
+        try {
+            executor.call(null, this.onResolve.bind(this), this.onReject.bind(this));
 
-finally() {}
+        } catch (e) {
+            this.errorHandler(e);
+        } finally {
+            this.finallyHandler();
+        }
+    }
+
+    onResolve(data) {
+        this.queue.forEach(callback => {
+            data = callback(data)
+        })
+        this.finallyHandler()
+    }
+
+    onReject(error) {
+        this.errorHandler(error);
+        this.finallyHandler();
+    }
+
+then(fn) {
+    this.queue.push(fn)
+    return this;
+}
+catch(fn) {
+    this.errorHandler = fn;
+    return this;
 }
 
- const promise = new Promise((resolve, reject) => {
+finally(fn) {
+    this.finallyHandler = fn
+    return this
+}
+}
+
+ const promise = new NadiiPromise((resolve, reject) => {
      setTimeout(() => {
     resolve('NgRx')
 }, 150)
-})
+});
+
 promise
-.then(course => {
-    console.log('Nadiia Promise', course)
-})
+.then(course => course.toUpperCase())
+.then(title => console.log('Vladilen Promise:', title))
 .catch(err => console.log('Error:', err))
 .finally(() => console.log ('Finally'));
 
-module.exports = NadiiPromise;
+
+module.exports = NadiiPromise; 
--- promise/promise.test.js
+++ promise/promise.test.js
@@ -4,29 +4,59 @@
 // npm instal jest
 const NadiiPromise = require('./promise.js');
 
+const t = setTimeout
+
 describe( 'Nadiia Promise', () => {
+    let promise;
+    let executorSpy;
+
+    const succesResalt = 42;
+    const errorResult = 'I am eeror';
+    beforeEach (() => {
+        executorSpy = jest.fn( r => t ( () => r(succesResalt), 150))
+        promise = new NadiiPromise(executorSpy)
+    });
 
  test('should exists und to be typeof function', () => {
 expect(NadiiPromise).toBeDefined();
 expect(typeof NadiiPromise).toBe('function');
 });
 test('instanse schould methods: then, catch, finally', () => {
-    const executorSpy = jest.fn(() => {});
-
-    const promise = new NadiiPromise(executorSpy);
-
+    
     expect(promise.then).toBeDefined();
     expect(promise.catch).toBeDefined();
     expect(promise.finally).toBeDefined();
 
 });
 test ('schould call executor function', () => {
-    const executor = jest.fn(() => {
-
-    })
-    const promise = new NadiiPromise(executorSpy)
+    
     expect(executorSpy).toHaveBeenCalled();
+});
+
+test('schould get data in this block and chain them', async() => {
+    const result = await promise.then(num => num).then(num => num  * 2)
+    expect(result).toBe(succesResalt * 2)
+});
+
+test ('schould catch error', () => {
+    const errorExecutor = (_, r) => t(() => r(errorResult),
+    150)
+    const errorPromise = new NadiiPromise(errorExecutor)
+    return new Promise( resolve => {
+        errorPromise.catch(error => {
+            expect(error).toBe(errorResult)
+            resolve()
+        })
+    })
+});
+
+test('schould call finally method', async () => {
+    const finallySpy =jest.fn(() => {});
+    await promise.finally(finallySpy);
+
+    expect(finallySpy).toHaveBeenCalled();
 })
+
 });
 
 //npm run test
