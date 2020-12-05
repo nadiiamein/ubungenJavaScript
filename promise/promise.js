7e49a8f511e9aa11af463de8867dbc47fdc7ad2a
Nadiia <npodufala15@gmail.com>
2020-12-05 11:46:22 +0200

    create promise

--- promise/promise.js
+++ promise/promise.js
@@ -1,7 +1,19 @@
 class NadiiPromise {
-
+then()
+catch()
+finally()
 }
 
- const promise = new
+ const promise = new Promise((resolve, reject) => {
+     setTimeout(() => {
+    resolve('NgRx')
+}, 150)
+})
+promise
+.then(course => {
+    console.log('Nadiia Promise', course)
+})
+.catch(err => console.log('Error:', err))
+.finally(() => console.log ('Finally'));
 
 module.exports = NadiiPromise;
